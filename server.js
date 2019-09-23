19cbe1ea6790c39d33dae38200d0527ac8bb1fa4
Aviral Verma <aviralverma.verma@gmail.com>
2019-09-23 22:21:50 +0530

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(___dirname + '/dist'));
+app.use(express.static(___dirname + '/dist/YouTube/'));
 
 app.listen(process.env.PORT || 4200);
 
